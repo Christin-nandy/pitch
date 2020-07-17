366f2d639f19fa290a9f15a55fff03dbe7fc2b7f
Christin-nandy <achiengchristine14@gmail.com>
2020-07-17 15:02:46 +0300

    add comment form

---
+++ app/main/forms.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField, SelectField, RadioField
+
+class CommentsForm(FlaskForm):
+    comment = TextAreaField('Comment', validators=[Required()])
+    vote=RadioField('default field arguments', choices=[('1', 'UpVote'), ('1', 'DownVote')])
+    submit = SubmitField('SUBMIT')  
