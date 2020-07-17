d5990c8fd7fe3b0ec597afd2bc2a586657fdd809
Christin-nandy <achiengchristine14@gmail.com>
2020-07-17 15:55:39 +0300

    add title and search pitch

---
+++ app/main/views.py
@@ -0,0 +1,15 @@
+from flask import render_template, request, redirect, url_for, abort  
+from . import main 
+from .forms import CommentsForm, UpdateProfile, PitchForm, UpvoteForm
+
+@main.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    title = 'Home - Welcome to one of the best pitching in the entire world'
+
+    search_pitch = request.args.get('pitch_query')
+    pitches= Pitch.get_all_pitches()  
+
+    return render_template('index.html', title = title, pitches= pitches)
